ce01e5ba40fa207eaadc2f4ee65f35f7000dc28f
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-04-18 05:15:53 +0200

    fetch & display sources and users

--- app.rb
+++ app.rb
@@ -1,5 +1,50 @@
 require 'sinatra/base'
 require 'torquebox'
+require 'faraday'
+require 'json'
+
+require "./config/settings.rb"
+
+module Sources
+  module_function
+
+  CONN = Faraday.new(:url => Settings::API+"sources")
+
+  def load
+    begin
+      resp = CONN.get do |req|
+        req.headers[:secret_session_key] = Settings::SECRET_SESSION_KEY
+      end
+    rescue Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed
+      raise
+    end
+
+    Array(JSON.parse(resp.body)["sources"])
+  end
+
+  def save
+    puts "save"
+  end
+end
+
+module Users
+  module_function
+
+  CONN = Faraday.new(:url => Settings::API+"users")
+
+  def load
+    begin
+      resp = CONN.get
+    rescue Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed
+      raise
+    end
+
+    Array(JSON.parse(resp.body)["reviewers"])
+  end
+
+  def save
+  end
+end
 
 class Brukerstyring < Sinatra::Base
 
@@ -9,6 +54,11 @@ class Brukerstyring < Sinatra::Base
   end
 
   get "/" do
+
+    @sources = Sources.load
+    users = Users.load
+    @sources.map { |s| s["users"] = users.group_by { |u| u["accountServiceHomepage"]}[s["uri"]] }
+
     erb :index
   end
 
